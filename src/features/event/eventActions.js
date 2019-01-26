3eca556e822c5feba280b5b1974b68bb8f1ed076
Prince K <k.prince1997@gmail.com>
2019-01-26 11:15:18 -0800

    Event Actions setup

---
+++ src/features/event/eventActions.js
@@ -0,0 +1,32 @@
+export const eventConstants = {
+  CREATE_EVENT: 'CREATE_EVENT',
+  UPDATE_EVENT: 'UPDATE_EVENT',
+  DELETE_EVENT: 'DELETE_EVENT'
+};
+
+export const createEvent = event => {
+  return {
+    type: eventConstants.CREATE_EVENT,
+    payload: {
+      event
+    }
+  };
+};
+
+export const updateEvent = event => {
+  return {
+    type: eventConstants.UPDATE_EVENT,
+    payload: {
+      event
+    }
+  };
+};
+
+export const deleteEvent = eventId => {
+  return {
+    type: eventConstants.DELETE_EVENT,
+    payload: {
+      eventId
+    }
+  };
+};
