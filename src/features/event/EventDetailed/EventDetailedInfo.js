91f0e7d69b62e51b4736ae0ed982dbeab6ab93cf
Prince K <k.prince1997@gmail.com>
2019-02-01 16:59:50 -0800

    Set up google map

--- src/features/event/EventDetailed/EventDetailedInfo.js
+++ src/features/event/EventDetailed/EventDetailedInfo.js
@@ -1,43 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Segment, Grid, Icon, Button } from 'semantic-ui-react';
+import EventDetailedMap from './EventDetailedMap';
 
-export default function EventDetailedInfo({ event }) {
-  const { description, date, venue } = event;
-  return (
-    <Segment.Group>
-      <Segment attached="top">
-        <Grid>
-          <Grid.Column width={1}>
-            <Icon size="large" color="teal" name="info" />
-          </Grid.Column>
-          <Grid.Column width={15}>
-            <p>{description}</p>
-          </Grid.Column>
-        </Grid>
-      </Segment>
-      <Segment attached>
-        <Grid verticalAlign="middle">
-          <Grid.Column width={1}>
-            <Icon name="calendar" size="large" color="teal" />
-          </Grid.Column>
-          <Grid.Column width={15}>
-            <span>{date}</span>
-          </Grid.Column>
-        </Grid>
-      </Segment>
-      <Segment attached>
-        <Grid verticalAlign="middle">
-          <Grid.Column width={1}>
-            <Icon name="marker" size="large" color="teal" />
-          </Grid.Column>
-          <Grid.Column width={11}>
-            <span>{venue}</span>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Button color="teal" size="tiny" content="Show Map" />
-          </Grid.Column>
-        </Grid>
-      </Segment>
-    </Segment.Group>
-  );
+class EventDetailedInfo extends Component {
+  state = {
+    showMap: false
+  };
+
+  toggleMap = () =>
+    this.setState(prevState => ({ showMap: !prevState.showMap }));
+
+  render() {
+    const { description, date, venue, venueLatLng } = this.props.event;
+    return (
+      <Segment.Group>
+        <Segment attached="top">
+          <Grid>
+            <Grid.Column width={1}>
+              <Icon size="large" color="teal" name="info" />
+            </Grid.Column>
+            <Grid.Column width={15}>
+              <p>{description}</p>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        <Segment attached>
+          <Grid verticalAlign="middle">
+            <Grid.Column width={1}>
+              <Icon name="calendar" size="large" color="teal" />
+            </Grid.Column>
+            <Grid.Column width={15}>
+              <span>{date}</span>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        <Segment attached>
+          <Grid verticalAlign="middle">
+            <Grid.Column width={1}>
+              <Icon name="marker" size="large" color="teal" />
+            </Grid.Column>
+            <Grid.Column width={11}>
+              <span>{venue}</span>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Button
+                color="teal"
+                size="tiny"
+                content={this.state.showMap ? 'Hide Map' : 'Show Map'}
+                onClick={this.toggleMap}
+              />
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        {this.state.showMap && (
+          <EventDetailedMap lat={venueLatLng.lat} lng={venueLatLng.lng} />
+        )}
+      </Segment.Group>
+    );
+  }
 }
+
+export default EventDetailedInfo;
---
+++ src/features/event/EventDetailed/EventDetailedMap.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Segment, Icon } from 'semantic-ui-react';
+
+const Marker = () => <Icon name="marker" size="big" color="red" />;
+
+function EventDetailedMap({ lat, lng }) {
+  const center = [lat, lng];
+  const zoom = 14;
+  return (
+    <Segment attached="bottom" style={{ padding: 0 }}>
+      {/* // Important! Always set the container height explicitly */}
+      <div style={{ height: '300px', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDCTSt_rLjoxGH_pAI71SWqGWq_9XvEbrs' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <Marker lat={lat} lng={lng} />
+        </GoogleMapReact>
+      </div>
+    </Segment>
+  );
+}
+
+export default EventDetailedMap;
