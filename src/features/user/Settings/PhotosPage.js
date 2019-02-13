e2d3069c13b490d0fd65edff19601c80c6d75a1f
Prince K <k.prince1997@gmail.com>
2019-02-13 13:18:58 -0800

    set up image upload

--- src/features/user/Settings/PhotosPage.js
+++ src/features/user/Settings/PhotosPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import {
   Image,
   Segment,
@@ -11,7 +13,10 @@ import {
 } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
 import Cropper from 'react-cropper';
+import { toastr } from 'react-redux-toastr';
 import 'cropperjs/dist/cropper.css';
+
+import { uploadProfileImage } from '../userActions';
 
 class PhotosPage extends Component {
   state = {
@@ -24,6 +29,21 @@ class PhotosPage extends Component {
   onDrop = files => {
     this.setState({ files: files, fileName: files[0].name });
   };
+
+  uploadImage = async () => {
+    try {
+      await this.props.uploadProfileImage(
+        this.state.image,
+        this.state.fileName
+      );
+      this.resetCrop();
+      toastr.success('Success', 'Photo has been uploaded!');
+    } catch (error) {
+      toastr.error('Error', error.message);
+    }
+  };
+
+  resetCrop = () => this.setState({ file: [], image: {} });
 
   cropImage = () => {
     if (typeof this.refs.cropper.getCroppedCanvas() === 'undefined') return;
@@ -87,10 +107,25 @@ class PhotosPage extends Component {
               style={headerStyles}
             />
             {this.state.files[0] && (
-              <Image
-                style={{ minHeight: '200px', minWidth: '200px' }}
-                src={this.state.cropResult}
-              />
+              <div>
+                <Image
+                  style={{ minHeight: '200px', minWidth: '200px' }}
+                  src={this.state.cropResult}
+                />
+                <Button.Group>
+                  <Button
+                    style={{ width: '100px' }}
+                    positive
+                    icon="check"
+                    onClick={this.uploadImage}
+                  />
+                  <Button
+                    style={{ width: '100px' }}
+                    icon="close"
+                    onClick={this.resetCrop}
+                  />
+                </Button.Group>
+              </div>
             )}
           </Grid.Column>
         </Grid>
@@ -123,4 +158,7 @@ const headerStyles = {
   marginBottom: '25px'
 };
 
-export default PhotosPage;
+export default connect(
+  null,
+  { uploadProfileImage }
+)(PhotosPage);
--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -23,3 +23,44 @@ export const updateProfile = profile => async (
     dispatch(asyncActionEnd());
   }
 };
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  const firestore = firebase.firestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = { name: fileName };
+
+  try {
+    // upload file to firebase storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get Url of the image
+    let downloadUrl = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+    // get userdoc
+    const user = await firebase.auth().currentUser;
+    await firestore
+      .collection('users')
+      .doc(user.uid)
+      .update({
+        photoURL: downloadUrl
+      });
+
+    await user.updateProfile({ photoURL: downloadUrl });
+    // add new photo to photos collection
+    return await firestore
+      .collection('users')
+      .doc(user.uid)
+      .collection('photos')
+      .add({
+        name: fileName,
+        url: downloadUrl
+      });
+  } catch (error) {
+    console.error(error);
+    throw new Error('Problem uploading photo');
+  }
+};
